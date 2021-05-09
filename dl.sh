a6811ed9d0b0eee7fce07c97f7072e58d67cda49
HamzaKerem <hamzamusic34@gmail.com>
2021-05-09 20:46:54 +0300

    Almost done.

--- dl.sh
+++ dl.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
 
 # POSIX compliant script for downloading content to a directory using youtube-dl.
-# Change configuration options to suit your needs.
+# Change configuration options to suit your needs. link_file MUST be modified.
 
 # Basic configuration
+link_file="/home/hkm/Programming/POSIX_Scripts/dl/dl.conf" # full path of file containing the links of the media
+# The file's first line should be the directory to which the content is to be downloaded, the rest should be links. See: dl_links function
+new_format="mp3" # downloaded content will be converted into this format
 ytdl_opt="-x" # parameter passed to youtube-dl
-link_file="/home/hkm/Programming/Bash_Scripts/dl.conf" # file containing the links of the media
-# link_file's first line should be the directory to which the content is to be downloaded; the rest should be links.
+lib_file="$(dirname $0)/util.sh" # No need to change
+
+# Source function library file
+. "$lib_file" || { printf "Unable to source $function_file. Exitting.\n" >&2 && exit 1; }
 
 # Error checking
-[ -z "$(which youtube-dl)" ] && printf "youtube-dl not in path. Exitting.\n" >&2 && exit 1
-[ -z "$(which ffmpeg)" ] && printf "ffmpeg not in path. Exitting.\n" >&2 && exit 1
-[ ! -r "$link_file" ] && printf "Unable to read link file. Exitting.\n" >&2 && exit 1
+[ -z "$(which youtube-dl)" ] && err "youtube-dl not in path."
+[ -z "$(which ffmpeg)" ] && err "ffmpeg not in path. Exitting."
+[ ! -r "$link_file" ] && err "Unable to read link file. Exitting."
 
 dl_links(){
 count=0
 while read line; do
 	if [ "$count" -eq 0 ]; then
-		dir="$line"
-		[ ! -d "$dir" ] || { mkdir "$dir" ||  { printf "Unable to make directory $dir. Exitting.\n" >&2 && exit 1; } }
+		content_dir="$line"
+		[ ! -d "$content_dir" ] || { mkdir "$content_dir" || err "Unable to make directory $content_dir."; }
 
 		# Download all links to temp directory 
-		temp_dir="$(mktemp -d)"
-		cd "$temp_dir"
+		temp_dir="$(mktemp -d)" && cd "$temp_dir"
+		[ "$?" -ne 0 ] && err "Unable to cd into temporary directory."
 	else
-		youtube-dl "$ytdl_opt" "$line"	
+#		youtube-dl "$ytdl_opt" "$line"	
+		echo "$line"
 	fi
 	count=$((count+1))
 
@@ -33,9 +39,17 @@ done < "$link_file"
 
 convert(){
 	for file in *; do
-		
+		new_file="$(chext $file $new_format)"
+		[ "$?" -ne 0 ] && err "$new_file"
+		ffmpeg -i "$file" "$new_file"
+	done
+}
+
 
 # Read links and dir from file. Download links to a temp dir.
 dl_links
+
+# Convert downloaded content into given format, new_format.
+convert
 
 rm -rf "$temp_dir"
